05ea85bca480d5d70de668c831d7ea335928564b
yhuu0078 <yhuu0078@student.monash.edu>
2024-09-01 22:14:49 +1000

    Finished W5

--- yhu-library/src/components/BHeader.vue
+++ yhu-library/src/components/BHeader.vue
@@ -26,7 +26,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { useRouter } from 'vue-router'
 
 const checkAuthentication = computed(() => {
